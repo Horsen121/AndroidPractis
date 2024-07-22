90457c2644c4480970418b50f84108884d30ff75
Horsen121 <vova.mitychin@mail.ru>
2024-07-22 23:30:21 +0700

    add: configure Gradle, Crime screen and VM, Crimes screen and VM, routes and Navigation; fix: ; delete:

--- app/src/main/java/com/androidlearn/criminalintent/MainActivity.kt
+++ app/src/main/java/com/androidlearn/criminalintent/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.androidlearn.criminalintent
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,62 +8,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.androidlearn.criminalintent.features.crime.CrimeScreen
-import com.androidlearn.criminalintent.features.crimes.CrimesScreen
 import com.androidlearn.criminalintent.ui.theme.CriminalIntentTheme
-import com.androidlearn.criminalintent.utils.Routes
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CriminalIntentTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    val viewModel: MainViewModel by lazy { MainViewModel(applicationContext) }
-
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = Routes.CrimesScreen.route
-                    ) {
-                        composable(Routes.MainScreen.route) {
-                            MainScreen()
-                        }
-                        composable(Routes.CrimesScreen.route) {
-                            CrimesScreen(
-                                context = applicationContext,
-                                navController = navController
-                            )
-                        }
-                        composable(
-                            Routes.CrimeScreen.route+ "?crimeId={crimeId}",
-                            arguments = listOf(
-                                navArgument(name = "crimeId") {
-                                    type = NavType.LongType
-                                    defaultValue = -1L
-                                },
-                            )
-                        ) {
-                            CrimeScreen(
-                                context = applicationContext,
-                                navController = navController
-                            )
-                        }
-                    }
+                    Greeting()
                 }
             }
         }
     }
+}
 
-    @Composable
-    fun MainScreen() {
+@Composable
+fun Greeting() {
 
-    }
 }
--- app/src/main/java/com/androidlearn/criminalintent/utils/Routes.kt
+++
@@ -1,7 +0,0 @@
-package com.androidlearn.criminalintent.utils
-
-sealed class Routes(val route: String) {
-    data object MainScreen: Routes("main_screen")
-    data object CrimesScreen: Routes("crimes_screen")
-    data object CrimeScreen: Routes("crime_screen")
-}
