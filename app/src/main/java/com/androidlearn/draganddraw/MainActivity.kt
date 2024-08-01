fc9082786c227c87d846e984d523f831a1094782
Horsen121 <vova.mitychin@mail.ru>
2024-08-01 11:20:03 +0700

    initial

---
+++ app/src/main/java/com/androidlearn/draganddraw/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.androidlearn.draganddraw
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.androidlearn.draganddraw.ui.theme.DragAndDrawTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DragAndDrawTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
